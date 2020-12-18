c7b2dedfa53aadc58e661ce4adad43e77d934910
andrew-roi <andriyroy3@gmail.com>
2020-12-18 19:03:31 +0200

    Finish the task.

--- src/com/it_school/lectureFour/Main.java
+++ src/com/it_school/lectureFour/Main.java
@@ -3,8 +3,11 @@ package com.it_school.lectureFour;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static java.lang.Math.abs;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -29,18 +32,45 @@ public class Main {
         } catch (IllegalArgumentException iae) {
             System.out.println("You should enter only literals. No others symbols");
         }
+        int[] array1 = {-4, -1, 0, 3, 10};
+        squareArraysValues(array1);
+        sortArray(array1);
     }
 
     static int numJewelsInStone(String jewels, String stone) {
 
-        Matcher pattern1 = Pattern.compile("a+").matcher(jewels);
+        Matcher pattern1 = Pattern.compile(jewels).matcher(stone);
 
-        if (pattern1.find()) {
-            String str = stone.substring(pattern1.start(), pattern1.end()+1);
-            return str.length();
-        } else return 0;
+        StringBuilder str = new StringBuilder();
+        while (pattern1.find()) {
 
+            str.append(stone, pattern1.start(), (pattern1.end()));
+        }
+        return str.length();
 
+    }
+
+    public static void squareArraysValues(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = abs((array[i] * array[i]));
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void sortArray(int[] array) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int bigger = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = bigger;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
 
 }
