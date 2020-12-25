3538ed86ffaeca83d37b6d91c13cb2d8c7d032a4
andrew-roi <andriyroy3@gmail.com>
2020-12-25 16:10:14 +0200

    Rework checkDomain() method. Now it returns string to main method.Change regular expressions to check date and time (Now it work correctly to task). Also rework Color enum with constructors in they objects

--- src/com/it_school/lecture06/Main.java
+++ src/com/it_school/lecture06/Main.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String regexpForDate = "((19|20)\\d\\d)/(0?[1-9]|1[012])/(0[1-9]|[12][0-9]|3[01]0?)\\s([01]?[0-9]|2[0-3]):[0-5][0-9]";
+        String regexpForDate = "((19|20)\\d\\d)/(0?[1-9]|1[012])/(0[1-9]|[12][0-9]|3[01]?)\\s([01]?[0-9]|2[0-3]):[0-5][0-9]";
         String inputDate = "2020/12/31 12:12";
         String inputDate1 = "2020/12/3112:12";
         String inputDate2 = "2020/12/31Z012:12";
