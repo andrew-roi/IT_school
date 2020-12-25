7cb052b5eab2e5f434eb4bba4f57c5986d60a9d6
andrew-roi <andriyroy3@gmail.com>
2020-12-25 16:21:32 +0200

    Commit feature-1 branch

--- src/com/it_school/lecture04/Main.java
+++ src/com/it_school/lecture04/Main.java
@@ -51,7 +51,7 @@ public class Main {
         }
         return calcGems;
     }
-
+        //some code
     public static void squareArraysValues(int[] array) {
 
         for (int i = 0; i < array.length; i++) {
