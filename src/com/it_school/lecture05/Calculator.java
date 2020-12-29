7cf18cc80e4ea04f48af2020d2fedcbf2e105750
andrew-roi <andriyroy3@gmail.com>
2020-12-29 10:51:07 +0200

    Add multiply method into the branch multiply

--- src/com/it_school/lecture05/Calculator.java
+++ src/com/it_school/lecture05/Calculator.java
@@ -1,15 +1,9 @@
 package com.it_school.lecture05;
 
 public class Calculator {
-    public int plus(int a, int b) {
-        return a + b;
+
+    public double multiply(double a, double b) {
+        return a * b;
     }
-    public int plus(int[] array){
-        int sum = 0;
-        for (int i:array
-        ) {
-            sum +=array[i];
-        }
-        return sum;
     }
-}
+
