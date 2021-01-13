591c53521198c960f41896425385585ee53bbc12
andrew-roi <andriyroy3@gmail.com>
2021-01-13 19:51:09 +0200

    Refactor method commonString(). Replaced with String.join()

--- src/com/it_school/lecture01/Task_3.java
+++ src/com/it_school/lecture01/Task_3.java
@@ -1,5 +1,7 @@
 package com.it_school.lecture01;
 
+
+
 /**
  * Написать метод, который принимает массив String и возвращает String,
  * в котором все строки из массива объединены в одну строку через разделитель “|”.
@@ -11,18 +13,11 @@ package com.it_school.lecture01;
  */
 public class Task_3 {
     public static void main(String[] args) {
-        System.out.println(commonString(new String[]{"ab", "cd", "ef", "dg", "rr", "ww"}));
+        System.out.println(commonString(new String[]{"ab"}));
 
     }
 
     public static String commonString(String[] s) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < s.length - 1; i++) {
-
-            stringBuilder.append(s[i]).append("|").append(s[++i]).append("|");
-
-        }
-        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-        return stringBuilder.toString();
+        return String.join("|",s);
     }
 }
