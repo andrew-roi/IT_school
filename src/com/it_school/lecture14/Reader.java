7af2c176177edda6214ebf4da8bc06927958d555
andrew-roi <andriyroy3@gmail.com>
2021-01-21 10:28:04 +0200

    Refactor run method() in class Reader.

--- src/com/it_school/lecture14/Reader.java
+++ src/com/it_school/lecture14/Reader.java
@@ -16,22 +16,22 @@ public class Reader implements Runnable {
     @Override
     public void run() {
         try {
+
             for (int i = 0; i < Integer.MAX_VALUE; i++) {
-                Thread.sleep(1_000);
-                System.out.println(list);
-                list.clear();
-
-
                 if (list.isEmpty()) {
                     LocalDate date = LocalDate.now();
                     System.out.println("For " + date + " the list is empty");
-                }
-            }
 
+                }
+
+                System.out.println(list);
+                list.clear();
+                Thread.sleep(1_000);
+
+            }
 
         } catch (InterruptedException ex) {
             System.out.println(ex.getMessage());
         }
-
     }
 }
