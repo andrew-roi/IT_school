b3bbd0bca9cc8b76f13bbf29cf1a66c8ea77a665
andrew-roi <andriyroy3@gmail.com>
2021-02-08 13:03:47 +0200

    Finished task for 1 thread and 10 mln elements and 2 threads and 5 mln elements. Time for 1 thread is about 27.5 sec, for 2 threads about 13.5 sec.

---
+++ src/com/it_school/lecture18/task1/MyArray2.java
@@ -0,0 +1,39 @@
+package com.it_school.lecture18.task1;
+
+public class MyArray2 implements Runnable {
+    String[] array;
+
+    MyArray2(String[] array) {
+        this.array = array;
+    }
+
+    @Override
+    public void run() {
+        try {
+            long start = System.currentTimeMillis();
+            for (int index = 5_000_000; index < 10_000_000; index++) {
+                if (index % 3 == 0 && index % 5 == 0) {
+                    array[index] = "FizzBuzz";
+                    System.out.println(array[index]);
+                } else if (index % 5 == 0) {
+                    array[index] = "Buzz";
+                    System.out.println(array[index]);
+                } else if (index % 3 == 0) {
+                    array[index] = "Fizz";
+                    System.out.println(array[index]);
+                } else {
+                    array[index] = String.valueOf(index);
+                    System.out.println(array[index]);
+                }
+
+
+            }
+            //code for calculating method working time
+            long end = System.currentTimeMillis();
+            long workTime = end - start;
+            System.out.println("One thread fill  of " + array.length + " mln elements array in " + (workTime / 1000.) + " sec");
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+}
