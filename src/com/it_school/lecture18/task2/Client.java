f4f84ea77e2cb9cb900c6a441464c92abfbd0bc3
andrew-roi <andriyroy3@gmail.com>
2021-02-09 21:06:01 +0200

    Task finished using Queue interface and class Timer. Task works in 3 threads. Time of generation new Client works quicker than deletion him.

--- src/com/it_school/lecture18/task2/Client.java
+++ src/com/it_school/lecture18/task2/Client.java
@@ -1,21 +1,12 @@
 package com.it_school.lecture18.task2;
 
-import java.util.Random;
-import java.util.TimerTask;
 
-public class Client extends TimerTask {
+public class Client  {
 
+    private static int countId;
 
-    @Override
-    public void run() {
-        Random random = new Random();
-        try {
-
-
-
-        } catch (Exception exception) {
-
-            System.out.println(exception.getMessage());
-        }
+    Client() {
+        ++countId;
     }
+
 }
