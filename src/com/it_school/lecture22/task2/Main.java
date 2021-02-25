97f462a8bafb868c600045e9fc407b271bae059d
andrew-roi <andriyroy3@gmail.com>
2021-02-25 11:53:34 +0200

    Add values for printing

--- src/com/it_school/lecture22/task2/Main.java
+++ src/com/it_school/lecture22/task2/Main.java
@@ -2,22 +2,30 @@ package com.it_school.lecture22.task2;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchFieldException {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         MyClass myClass = new MyClass();
         printObjectsFields(myClass);
-
     }
 
-    public static void printObjectsFields(Object obj) {
+
+    public static void printObjectsFields(Object obj) throws IllegalAccessException {
 
         Field[] fields = obj.getClass().getDeclaredFields();
+        System.out.println(Arrays.toString(fields));
         for (Field f : fields) {
             f.setAccessible(true);
-            int modifiers = f.getModifiers();
-            System.out.println(Modifier.toString(modifiers) + "|" + f.getType() + "|" + f.getName());
         }
+
+        int modifiers = obj.getClass().getModifiers();
+        for (Field field : fields) {
+            Object value = field.get(obj);
+            System.out.println(Modifier.toString(modifiers) + "|" + value.getClass().getName()
+                    + "|" + value);
+        }
+
 
     }
 }
--- src/com/it_school/lecture22/task2/MyClass.java
+++ src/com/it_school/lecture22/task2/MyClass.java
@@ -1,6 +1,6 @@
 package com.it_school.lecture22.task2;
 
 public class MyClass {
-   private String name;
-   protected static int id;
+   private String name = "Andrii";
+   protected static int id = 1;
 }
