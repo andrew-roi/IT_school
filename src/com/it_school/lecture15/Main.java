32281665289a07b7d8cda47ee8b36080d20aad57
andrew-roi <andriyroy3@gmail.com>
2021-03-03 12:49:18 +0200

    Refactor.

--- src/com/it_school/lecture15/Main.java
+++ src/com/it_school/lecture15/Main.java
@@ -2,6 +2,8 @@ package com.it_school.lecture15;
 
 public class Main {
     public static void main(String[] args) {
-
+    Customer customer1 = new Customer();
+    Order order1 = new NormalOrder();
+    Order order2 = new SpecialOrder();
     }
 }
