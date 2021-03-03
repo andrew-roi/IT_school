c1b7e5c34fd8a4bf23af7f4f402d8704ec93874d
andrew-roi <andriyroy3@gmail.com>
2021-03-03 12:55:40 +0200

    Added description.

--- src/com/it_school/lecture22/task1/Main.java
+++ src/com/it_school/lecture22/task1/Main.java
@@ -2,6 +2,17 @@ package com.it_school.lecture22.task1;
 
 import java.lang.reflect.Field;
 
+/**
+ * 1. С помощью рефлексии изменить значение строки "hello".
+ *
+ * Ожидаемое решение:
+ *
+ * String s = "hello";
+ * // some work with reflection
+ * System.out.println(s); // print "ABCDE"
+ *
+ * Решение s = "ABCDE" - не засчитывается.
+ */
 public class Main {
 
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
--- src/com/it_school/lecture22/task2/Main.java
+++ src/com/it_school/lecture22/task2/Main.java
@@ -6,6 +6,15 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
+/**
+ * 2. Написать метод, который принимает любой объект и печатает в консоль значения его полей в формате:
+ *
+ * модификаторы поля | имя_поля | значение
+ *
+ * Модификаторы должны быть в читаемом виде (public, protected), а не в виде чисел (1, 3,4)
+ * 3. Написать аннотацию @Ignore, которая может применяться только к полям класса.
+ * Применить ее к заданию 2: если поле помечено @Ignore - не печатать его в консоль.
+ */
 public class Main {
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         MyClass myClass = new MyClass();
