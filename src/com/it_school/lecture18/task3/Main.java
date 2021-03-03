c8cdd22495567b8b03f8f1878c603c7d3fc427e7
andrew-roi <andriyroy3@gmail.com>
2021-03-03 12:51:17 +0200

    Added description.

--- src/com/it_school/lecture18/task3/Main.java
+++ src/com/it_school/lecture18/task3/Main.java
@@ -1,5 +1,15 @@
 package com.it_school.lecture18.task3;
 
+/**
+ * Написать класс MyDate. В нем должны быть определены статические классы Month и DayOfWeek. Поля класса MyDate:
+ * Month month;
+ * int day;
+ * DayOfWeek dayOfWeek;
+ * Создать несколько объектов MyDate с помощью конструкторов:
+ * MyDate(Month, int day)
+ * MyDate(Month, int day, DayOfWeek dayOfWeek)
+ * Подумайте, можно ли сделать вложенные классы перечислениями (энамами).
+ */
 public class Main {
     public static void main(String[] args) {
         MyDate date1 = new MyDate(MyDate.Month.January,2);
